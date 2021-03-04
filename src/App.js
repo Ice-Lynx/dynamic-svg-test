385a6d6310f300b230f38e8c6bffe7ada03386b9
P Y <py@Zs-MacBook-Pro.local>
2021-03-04 23:53:26 +0200

    something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Icon from "./Icon";
 
-function App() {
+import "./style.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How to use svg icons in React</h1>
+      <hr />
+      <div>
+        {/* simply use <Icon /> to display svg as React Component  */}
+        <Icon className="icon" name="unicorn" />
+        <Icon className="icon" name="lion" />
+        <Icon className="icon" name="dwarf" />
+      </div>
     </div>
   );
 }
-
-export default App;
